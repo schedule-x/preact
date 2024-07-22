671da557cce15f592e65c41673fc5c122895b815
Tom Österlund <tom.osterlund1@gmail.com>
2024-07-22 21:31:18 +0200

    ci: edit build process

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,16 @@ import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [
+      preact(),
+  ],
+  build: {
+    lib: {
+      entry: 'src/index.tsx',
+      name: 'ScheduleXPreact',
+    },
+    rollupOptions: {
+      external: ['preact', '@schedule-x/calendar'],
+    }
+  },
 })
