ccc9211459800ca2dfce8dfd394b193999a4686d
Tom Österlund <tom.osterlund1@gmail.com>
2024-07-22 19:51:03 +0200

    fix: use correct syntax when initializing useState in custom hook

--- src/use-calendar-app.tsx
+++ src/use-calendar-app.tsx
@@ -1,22 +1,9 @@
-import {CalendarApp, CalendarConfig, createCalendar,} from '@schedule-x/calendar'
-import {useEffect, useState} from 'preact/hooks'
-import {signal} from '@preact/signals'
-
-const calendarApp = signal<CalendarApp | null>(null);
+import {CalendarConfig, createCalendar} from '@schedule-x/calendar'
+import {useState} from "preact/hooks";
 
 export function useCalendarApp(config: CalendarConfig) {
-  if (!calendarApp.value) calendarApp.value = createCalendar(config);
-  return calendarApp.value;
-}
-
-export function useNextCalendarApp(config: CalendarConfig) {
-  const [calendarApp, setCalendarApp] = useState<CalendarApp>()
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setCalendarApp(createCalendar(config))
-    }
-  }, [])
+  const [calendarApp] = useState(() => createCalendar(config))
 
   return calendarApp
 }
+
