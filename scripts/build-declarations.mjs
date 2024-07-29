7faf84b7b6e3426e7055bd71388a6b11fcec5455
Tom Österlund <tom.osterlund1@gmail.com>
2024-07-29 09:06:13 +0200

    fix: build script

--- scripts/build-declarations.mjs
+++ scripts/build-declarations.mjs
@@ -2,6 +2,7 @@ import { execSync } from 'child_process'
 
 function build() {
   try {
+    execSync('tsc --project tsconfig.build.json')
     const content = execSync('cat dist/types/index.d.ts').toString()
     const updatedContent = content.replace(/\.tsx/g, '.d.ts')
     execSync(`echo "${updatedContent}" > dist/types/index.d.ts`)
