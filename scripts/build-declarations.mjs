b3f3d7cce8e40db7f107e632f364143786e7f599
Tom Österlund <tom.osterlund1@gmail.com>
2024-07-29 09:01:06 +0200

    fix: declaration build script

--- scripts/build-declarations.mjs
+++ scripts/build-declarations.mjs
@@ -2,7 +2,6 @@ import { execSync } from 'child_process'
 
 function build() {
   try {
-    execSync('vue-tsc --project tsconfig.build.json')
     const content = execSync('cat dist/types/index.d.ts').toString()
     const updatedContent = content.replace(/\.tsx/g, '.d.ts')
     execSync(`echo "${updatedContent}" > dist/types/index.d.ts`)
